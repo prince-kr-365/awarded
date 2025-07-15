e2b5fc845a8f88bbebcede024374decf886b2e50
Prince <dkanshu952@gmail.com>
2025-07-15 08:24:34 +0530

    Remove unwanted folders, keep only awarded

--- WelcomeTOjs/introdemo.js
+++
@@ -1,7 +0,0 @@
-const yourevent = new Event("gharJao");
-
-document.querySelector("button")
-.addEventListener("gharJao", function(){
-    alert("jaa raha hu ghar");
-})
-document.querySelector("button").dispatchEvent(yourevent);
